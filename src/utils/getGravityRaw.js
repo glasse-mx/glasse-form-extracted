5b85b684133a6cb85c6c775c2c2257319a6123f7
Gabriel Coronado <gcoronado5100@gmail.com>
2023-07-13 17:59:06 -0600

    hola

--- src/utils/getGravityRaw.js
+++ src/utils/getGravityRaw.js
@@ -2,29 +2,33 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { getAuthorizationHeader } from "../hooks/useOAuthSignature" 
 
-export const geGravityRaw = (id) => {
+export const geGravityRaw = async (id) => {
   const url = import.meta.env.VITE_PUBLIC_WORDPRESS_URL;
   const [gravityForm, setGravityForm] = useState({});
 
 
   const options = {
-    method: "get",
+    method: "GET",
+    maxBodyLength: Infinity,
     url: `${url}${id}`,
     headers: {
-      'Authorization': getAuthorizationHeader('GET','1')
+      Authorization: getAuthorizationHeader('GET','1'),
+      'Content-Tpe': 'application/json'
     },
   };
 
-  useEffect(() => {
-    axios
-      .request(options)
-      .then(function (response) {
-        setGravityForm(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .request(options)
+  //     .then(function (response) {
+  //       setGravityForm(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  // }, []);
 
+  const formulari0 = await axios.get('https://app.glasse.com.mx/wp-json/gf/v2/forms/1',options)
+  console.log(formulari0)
   return gravityForm;
 };
